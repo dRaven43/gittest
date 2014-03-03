094a2c273e0b3bfe9c86c10578c5df04fab89e99
David Prater <davidvprater@gmail.com>
2014-03-03 09:04:29 -0600

    Added Foo Class

---
+++ Foo.php
@@ -0,0 +1,20 @@
+<?php
+
+class foo {
+
+	private a;
+	private b;
+
+	public function seta($value){
+		$this->a = $value;
+	} // end function seta 
+
+	public function setb($value){
+		$this->b = $value;
+	} // end function setb
+
+
+} // end class foo
+
+
+?>
